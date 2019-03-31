cb8ae8056ee06eb0eb881138cdd27b22ceff60ee
chrobahs <47793145+chrobahs@users.noreply.github.com>
2019-03-31 16:28:31 -0500

    Add files via upload

---
+++ halloween.js
@@ -0,0 +1,5 @@
+alert("Hello Halloween Fans!");
+var person = prompt("Please enter your name:","First Name & Last Name");
+var email = prompt("Please enter your email for update:","13halloween@gmail.com");
+alert("Thanks for subscribing "+person+". We will be sending you updates at "+email);
+console.log(document.images);
